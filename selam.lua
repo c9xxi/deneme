2276c018a265c8549dd45e63f6394be0055e3df2
c9xxi <omurtasci45@gmail.com>
2025-07-16 19:29:47 +0300

    Update selam.lua

--- selam.lua
+++ selam.lua
@@ -1,1 +1,43 @@
+-- StarterGui içine bir ScreenGui ekle ve içine bir Frame, TextLabel, TextBox, TextButton koy
+-- Aşağıdaki script, Frame içine eklenmeli
+
+local frame = script.Parent
+
+-- Sürüklenebilir yapmak için basit kod:
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+    local delta = input.Position - dragStart
+    frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+                              startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = true
+        dragStart = input.Position
+        startPos = frame.Position
+        
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement then
+        dragInput = input
+    end
+end)
+
+game:GetService("UserInputService").InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
 
